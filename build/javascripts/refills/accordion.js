ff158fb33381f6908291ca982b8e84d274a094b3
menslow <michael.enslow@gmail.com>
2014-09-12 12:42:27 -0700

    removing build files from project

--- build/javascripts/refills/accordion.js
+++
@@ -1,5 +0,0 @@
-$('.js-accordion-trigger').bind('click', function(){
-  jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
-  jQuery(this).parent().toggleClass('is-expanded');
-  event.preventDefault();
-});
